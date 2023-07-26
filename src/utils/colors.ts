75cc391d6005e656ac4d9a8aa8ec879d1a1be726
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-07-26 15:57:37 -0300

    Initial commit

---
+++ src/utils/colors.ts
@@ -0,0 +1,33 @@
+export const typeColors = {
+agua: '#5090D6',
+dragao: '#0B6DC3',
+eletrico: '#F4D23C',
+fada: '#EC8FE6',
+fantasma: '#5269AD',
+fogo: '#FF9D55',
+gelo: '#73CEC0',
+grama: '#63BC5A',
+inseto: '#91C12F',
+lutador: '#CE416B',
+normal: '#919AA2',
+noturno: '#5A5465',
+metal: '#5A8EA2',
+pedra: '#C5B78C',
+psíquico: '#FA7179',
+terrestre: '#D97845',
+venenoso: '#B567CE',
+voador: '#89AAE3',
+}
+
+/* --escala-de-cinza---preto: #000000;
+--escala-de-cinza--900: #1A1A1A;
+--escala-de-cinza--800: #333333;
+--escala-de-cinza--700: #4D4D4D;
+--escala-de-cinza--600: #666666;
+--escala-de-cinza--500: #808080;
+--escala-de-cinza--400: #999999;
+--escala-de-cinza--300: #B3B3B3;
+--escala-de-cinza--200: #CCCCCC;
+--escala-de-cinza--100: #E6E6E6;
+--escala-de-cinza--50: #F2F2F2;
+--escala-de-cinza---branco: #FFFFFF; */
