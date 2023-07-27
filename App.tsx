ed08f3b3b7688a9f3f0a5688c8de108f9e1f236d
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-07-27 18:01:37 -0300

    ajustes
    ajustes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import {Routes, theme} from './src';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <ThemeProvider theme={theme}>
+     <StatusBar style="auto" />
+     <Routes />
+   </ThemeProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
