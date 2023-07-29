6e5fd00b50f910abcada3bd6c13a3bec711179e7
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-07-29 09:39:02 -0300

    ajustes nos tamanhos
    ajustes nos tamanhos

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -1,6 +1,6 @@
 import Bug from './bug.svg'
 import Dark from './dark.svg'
-import Dragon from './bug.svg'
+import Dragon from './dragon.svg'
 import Eletric from './eletric.svg'
 import Fairy from './fairy.svg'
 import Fighting from './fighting.svg'
@@ -8,7 +8,7 @@ import Fire from './fire.svg'
 import Flying from './flying.svg'
 import Ghost from './ghost.svg'
 import Grass from './grass.svg'
-import Ground from './bug.svg'
+import Ground from './ground.svg'
 import Ice from './ice.svg'
 import Normal from './normal.svg'
 import Poison from './poison.svg'
@@ -16,14 +16,20 @@ import Psychic from './psychic.svg'
 import Rock from './rock.svg'
 import Steel from './steel.svg'
 import Water from './water.svg'
+import {SvgProps} from 'react-native-svg';
+
+type Proprs = {
+    width?: SvgProps['width']
+    height?: SvgProps['height']
+}
 
 export const svgs = {
     bug: <Bug />,
     dark: <Dark />,
     dragon: <Dragon />,
-    eletric: < Eletric />,
-    fairy: <Fairy />,
+    electric: < Eletric />,
     fighting: <Fighting />,
+    fairy: <Fairy />,
     fire: <Fire />,
     flying: <Flying />,
     ghost: <Ghost />, 
