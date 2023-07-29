cb437013cd5be0c3be96f97a2e9034496794d0e3
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-07-29 10:51:09 -0300

    ajustes

---
+++ src/assets/svgsPadrao/index.tsx
@@ -0,0 +1,60 @@
+import Bug from './bug.svg'
+import Dark from './dark.svg'
+import Dragon from './dragon.svg'
+import Eletric from './eletric.svg'
+import Fairy from './fairy.svg'
+import Fighting from './fighting.svg'
+import Fire from './fire.svg'
+import Flying from './flying.svg'
+import Ghost from './ghost.svg'
+import Grass from './grass.svg'
+import Ground from './ground.svg'
+import Ice from './ice.svg'
+import Normal from './normal.svg'
+import Poison from './poison.svg'
+import Psychic from './psychic.svg'
+import Rock from './rock.svg'
+import Steel from './steel.svg'
+import Water from './water.svg'
+
+import {SvgProps} from 'react-native-svg';
+
+type Proprs = {
+    width?: SvgProps['width']
+    height?: SvgProps['height']
+}
+
+export const svgs = {
+    bug: <Bug />,
+    dark: <Dark />,
+    dragon: <Dragon />,
+    electric: < Eletric />,
+    fighting: <Fighting />,
+    fairy: <Fairy />,
+    fire: <Fire />,
+    flying: <Flying />,
+    ghost: <Ghost />, 
+    grass: <Grass />,
+    ground:  <Ground />,
+    ice: <Ice />,
+    normal: <Normal /> ,
+    poison: <Poison />, 
+    psychic: <Psychic />,
+    rock: <Rock />,
+    steel: <Steel />,
+    water: <Water /> 
+}
+
+ 
+
+ 
+
+
+
+ 
+
+
+ 
+ 
+ 
+
