a075370aa2767c40eae94c00f6e7c6ed04b7436b
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-08-02 18:02:10 -0300

    iniciado search button
    iniciado search button

--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, BarraPesquisa } from './styles';
 
+
 export const InputSearch: React.FC = () => {
+  const [pokemon, setPokemon] = useState<string>('');
+
+
+  function handleSearchPokemon(search: string | number ){
+      console.log(typeof search)
+  }
+
   return (
     <Container>
-      <BarraPesquisa placeholder='Procurar' />
+      <BarraPesquisa placeholder='Procurar' 
+      onChangeText={ e => setPokemon(e)} 
+      value={pokemon}
+      />
     </Container>
   )
 }
